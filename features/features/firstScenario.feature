8f056a503e023af09d710f1b777e4b52b9fc381c
Veronika <nika1011@inbox.lv>
2022-05-02 18:48:49 +0300

    adding navigation steps

--- features/features/firstScenario.feature
+++ features/features/firstScenario.feature
@@ -3,15 +3,15 @@ Feature: First Scenario
   Scenario: Atempt to register with already registered user
     Given I open landing page
     And I have registered account
-    # And I have logged out
-    # When I press on Account button
-    # And I press log in
-    # When I press not yet a customer
-    # And I enter email address
-    # And I enter password
-    # And I enter password reapeatedly
-    # And I select security question about elder sibling
-    # And I enter answer
-    # And I press register button
-    # Then I see message about email must be unique
-    # And I am in register page
+    And I have logged out
+    When I press on Account button
+    And I press log in
+    When I press not yet a customer
+    And I enter email address
+    And I enter password
+    And I enter password reapeatedly
+    And I select security question about elder sibling
+    And I enter answer
+    And I press register button
+    Then I see message about email must be unique
+    And I am in register page
--- features/step-definitions/navigationSteps.js
+++ features/step-definitions/navigationSteps.js
@@ -1,4 +1,4 @@
-const { Given } = require('@wdio/cucumber-framework');
+const { Given, When, Then } = require('@wdio/cucumber-framework');
 
 Given(/^I open landing page$/, function () {
     browser.url("http://demo.owasp-juice.shop/#/");
@@ -6,3 +6,41 @@ Given(/^I open landing page$/, function () {
     // this.otherTest.dismissInitialPopus();
 });
 
+When (/^I have registered account$/, function () {   
+});
+
+When (/^I have logged out$/, function () {    
+});
+
+When (/^I press on Account button$/, function () {    
+});
+
+When (/^I press log in$/, function () {    
+});
+
+When (/^I press not yet a customer$/, function () {    
+});
+
+When (/^I enter email address$/, function () {    
+});
+
+When (/^I enter password$/, function () {    
+});
+
+When (/^I enter password reapeatedly$/, function () {    
+});
+
+When (/^I select security question about elder sibling$/, function () {    
+});
+
+When (/^I enter answer$/, function () {    
+});
+
+When (/^I press register button$/, function () {    
+});
+
+Then (/^I see message about email must be unique$/, function () {    
+});
+
+When (/^I am in register page$/, function () {    
+});
